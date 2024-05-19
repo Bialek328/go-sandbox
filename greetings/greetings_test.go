a9b80e6132e6d1f771b54e287b221b3a978722cb
Mateusz <bialek328@gmail.com>
2024-05-19 21:11:51 +0200

    printing shapes in console

--- greetings/greetings_test.go
+++ greetings/greetings_test.go
@@ -8,7 +8,7 @@ import (
 func TestHelloName(t *testing.T) {
 	name := "Mat"
 	want := regexp.MustCompile(`\b` + name + `\b`)
-	msg, err := Hello("Mat")
+	msg, err := Hello("Gladys")
 	if !want.MatchString(msg) || err != nil {
 		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
 	}
--- hello/hello.go
+++ hello/hello.go
@@ -1,22 +1,79 @@
 package main
 
 import (
-	"example.com/greetings"
+	// "example.com/greetings"
 	"fmt"
-	"log"
+	// "log"
 )
 
-func main() {
-	// setup logger
-	log.SetPrefix("greetings: ")
-	log.SetFlags(3)
+func printPyramid(rows int) {
+	spaces_nr := rows - 1
+	for i := 1; i <= rows; i++ {
+		for j := 1; j <= spaces_nr; j++ {
+			fmt.Print(" ")
 
-	names := []string{"Ania", "Paweł", "Dominik", "Ewa"}
-	hellos, err := greetings.GreetMultiplePeople(names)
-	if err != nil {
-		log.Fatal(err)
+		}
+		for k := 1; k <= 2*i-1; k++ {
+			fmt.Print("*")
+		}
+		spaces_nr -= 1
+		fmt.Println()
 	}
-	for _, greet := range hellos {
-		fmt.Println(greet)
+}
+
+func printSquare(rows int) {
+	for i := 1; i <= rows; i++ {
+		for j := 1; j <= rows; j++ {
+			fmt.Print("* ")
+		}
+		fmt.Println()
 	}
+}
+
+func printRectangle(rows, columns int) {
+	str := "* "
+	for i := 1; i <= rows; i++ {
+		for j := 1; j <= columns; j++ {
+			fmt.Print(str)
+		}
+		fmt.Println()
+	}
+}
+
+func chooseFigure() {
+	var figure string
+	var rows int
+	fmt.Println("Choose one figure: rectangle, square, pyramid:")
+	_, err1 := fmt.Scanln(&figure)
+	if err1 != nil {
+		fmt.Println(err1)
+		return
+	}
+	fmt.Println("How many rows do you want the figure to have?:")
+	_, err2 := fmt.Scanln(&rows)
+	if err2 != nil {
+		fmt.Println(err2)
+		return
+	}
+	switch figure {
+	case "rectangle":
+		var columns int
+		fmt.Println("How many columns do you want the figure to have?")
+		_, err := fmt.Scanln(&columns)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		printRectangle(rows, columns)
+	case "pyramid":
+		printPyramid(rows)
+	case "square":
+		printSquare(rows)
+	default:
+		fmt.Println("Wrong shape type")
+	}
+}
+func main() {
+
+	chooseFigure()
 }
